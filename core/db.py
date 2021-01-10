c5eaac117597da02b6dede2d89994b9fe9f72aea
jenifly <1439916120@qq.com>
2021-01-10 20:27:03 +0800

    .

--- core/db.py
+++ core/db.py
@@ -7,25 +7,25 @@ from databases import Database
 from core.config import DB_MYSQL_CONFIG, DB_REDIS_CONFIG
 
 
+class Database(Database):
+    async def insert(self, table: str, row: dict, duplicate: bool = False):
+        sql = f"""
+                insert into {table}
+                ({','.join(row.keys())})
+                values ('{"','".join(row.values())}')
+                """
+        if duplicate:
+            sql += f""" ON DUPLICATE KEY UPDATE
+                        {','.join(f"{k}='{v}'" for k,v in row.items())}
+                    """
+        await self.execute(sql)
+
+
 class DB:
     async def init(self):
         self.mysql = Database(**DB_MYSQL_CONFIG)
         await self.mysql.connect()
         self.redis = await aioredis.create_redis_pool(**DB_REDIS_CONFIG)
-
-        async def insert(table: str, row: dict, duplicate: bool = False):
-            _sql = f"""
-                    insert into {table}
-                    ({','.join(row.keys())})
-                    values ('{"','".join(row.values())}')
-                    """
-            if duplicate:
-                _sql += f""" ON DUPLICATE KEY UPDATE
-                            {','.join(f"{k}='{v}'" for k,v in row.items())}
-                        """
-            await self.mysql.execute(_sql)
-
-        self.mysql.insert = insert
 
     async def close(self):
         await self.mysql.disconnect()
