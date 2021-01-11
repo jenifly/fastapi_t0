85755886e2dbd34f52003e274703edee09376392
Jenifly <1439916120@qq.com>
2021-01-11 08:20:59 +0800

    .

--- model/response.py
+++ model/response.py
@@ -2,13 +2,21 @@ from typing import Any
 
 from pydantic import BaseModel
 
-RESPONSE_STATUS_MAP = {4000: "OK"}
-
-
-def RESPONSE_STATUS(status):
-    return {"status": status, "detail": RESPONSE_STATUS_MAP[status]}
-
 
 class ResponseModel(BaseModel):
-    status: int = 4000
-    detail: Any = 'OK'
+    code: int = 4000
+    result: Any
+
+
+__RESPONSE_STATUS_MAP = {
+    4000: 'success',
+    4001: '您的账号已在其他设备登录，请确认账户安全，是否继续登录？',
+}
+
+
+def RESPONSE_CODE(code) -> dict:
+    return {'code': code, 'result': __RESPONSE_STATUS_MAP[code]}
+
+
+def RESPONSE_RESULT(result) -> dict:
+    return {'code': 4000, 'result': result}
